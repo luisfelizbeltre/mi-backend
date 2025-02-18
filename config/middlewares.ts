bafc1a27064f630cd69d81513b1fb884127ea6f7
luis <luisfelizbeltre@gmail.com>
2025-02-18 15:46:36 +0100

    mejoras

--- config/middlewares.ts
+++ config/middlewares.ts
@@ -10,3 +10,13 @@ export default [
   'strapi::favicon',
   'strapi::public',
 ];
+module.exports = [
+  {
+    name: "strapi::cors",
+    config: {
+      enabled: true,
+      headers: "*",
+      origin: ["https://mi-frontend.vercel.app"]
+    },
+  },
+];
